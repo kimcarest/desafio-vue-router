57cfb7685b59b0867e7dc92347829108caaada6b
kimcarest <kcarestoledo@gmail.com>
2024-09-04 17:20:46 -0400

    🎉 init: subiendo

---
+++ src/views/ContactView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold text-body-emphasis">Contacto</h1>
+        <div class="col-lg-6 mx-auto">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="text-center mb-3">
+        
+        <router-link 
+            class="btn btn-outline-success fs-1"
+            to="/"
+        >Volver al home</router-link>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    
+}
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,46 @@
+<template>
+    <Card 
+      :data="{
+          nombre: nombre,
+          descripcion: descripcion,
+          precio: precio,
+          cantidad: cantidad,
+      }"
+    />
+
+    <div class="text-center mb-3">
+      <router-link 
+          class="btn btn-outline-success fs-1"
+          to="/"
+      >Volver al home</router-link>
+  </div>
+</template>
+
+<script>
+
+import Card from '../components/Card.vue'
+
+export default {
+  components: {
+      Card
+  },
+  props: {
+      nombre: {
+          type: String,
+          required: true
+      },
+      descripcion: {
+          type: String,
+          required: true
+      },
+      precio: {
+          type: String,
+          required: true
+      },
+      cantidad: {
+          type: String,
+          required: true
+      },
+  }
+}
+</script>
